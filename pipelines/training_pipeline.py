9b6d2cf362c97402284e52c8218606c79c276a74
fatima-ishaq <fatimaishaqabc@gmail.com>
2026-06-06 01:25:51 +0500

    fixed misc

--- pipelines/training_pipeline.py
+++ pipelines/training_pipeline.py
@@ -192,8 +192,7 @@ def train_model_for_day(df: pd.DataFrame, day_ahead: int):
         ("rf", RandomForestRegressor(n_estimators=200, max_depth=15, random_state=42)),
         ("xgb", XGBRegressor(n_estimators=200, max_depth=6, random_state=42)),
         ("gb", GradientBoostingRegressor(n_estimators=200, max_depth=4, random_state=42)),
-    ]),  # ← Make sure closing parenthesis is here
-}
+    ]),  }
     
     all_metrics = {}
     best_model, best_rmse, best_name = None, float("inf"), ""
